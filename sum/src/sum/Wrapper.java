1916e2e2e1b00bf8e5af20553c169af0399fee1e
hai <hai@LAPTOP-4DV9N6FJ>
2021-07-31 13:11:11 +0530

    Fifth Commit
    Uploading All Files

---
+++ sum/src/sum/Wrapper.java
@@ -0,0 +1,15 @@
+package sum;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=100;
+Integer obj1=new Integer(a);
+Integer obj2=Integer.valueOf(a);
+Integer obj3=a;
+System.out.println(obj1+ " "+obj2+ " "+obj3);
+
+	}
+
+}
